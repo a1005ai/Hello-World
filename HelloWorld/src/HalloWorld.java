72e43d0a8b921159599d742bf000294925eb456c
Architecture_aiityc <Architecture_aiityc@10.100.40.102>
2011-08-27 15:33:33 +0900

    JoHO

--- HelloWorld/src/HalloWorld.java
+++ HelloWorld/src/HalloWorld.java
@@ -2,5 +2,7 @@
 public class HalloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("The world");
+        System.out.println("Star Platinum");
     }
 }
